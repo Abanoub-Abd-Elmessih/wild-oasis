0d6d73a3e608b282b5508326fc0a6d17ceadabe5
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2024-12-29 18:18:41 +0200

    Creating cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,22 +1,28 @@
 import supabase from "./supabase";
 
+function handleSupabaseError(error, message) {
+  if (error) {
+    throw new Error(message);
+  }
+}
+
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
-  if (error) {
-    console.log(error);
-    throw new Error("Cabins could not be loaded");
-  }
+  handleSupabaseError(error, "Cabins could not be loaded");
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+  handleSupabaseError(error, "Cabins could not be created");
   return data;
 }
 
 export async function deleteCabin(id) {
-  const { data, error } = await supabase
-    .from("cabins")
-    .delete()
-    .eq("id",id);
-    if (error) {
-      console.log(error);
-      throw new Error("Cabins could not be deleted");
-    }
-    return data;
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  handleSupabaseError(error, "Cabins could not be deleted");
+  return data;
 }
