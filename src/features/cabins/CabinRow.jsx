f88a80b9c9dfc9f350f4d5fd2b1bedd24d577a1c
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2025-01-01 16:24:04 +0200

    Add Toggle list

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,18 +8,20 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -62,7 +64,7 @@ export default function CabinRow({ cabin }) {
   } = cabin;
 
   const { deleteCabin, isDeleting } = useDeleteCabin();
-  const { createCabin, isCreating } = useCreateCabin();
+  const { createCabin } = useCreateCabin();
   function handleDuplicate() {
     createCabin({
       name: `Copy of ${name}`,
@@ -76,7 +78,7 @@ export default function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row>
         {image ? <Img src={image} alt="cabin image" /> : "There's No Image"}
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity} guests</div>
@@ -86,7 +88,7 @@ export default function CabinRow({ cabin }) {
         ) : (
           <span>&mdash;</span>
         )}
-        <div>
+        {/* <div>
           <button disabled={isCreating} onClick={handleDuplicate}>
             <HiSquare2Stack />
           </button>
@@ -96,17 +98,47 @@ export default function CabinRow({ cabin }) {
           <button onClick={() => {setShowDeleteModal(true)}} disabled={isDeleting}>
             <HiTrash />
           </button>
-        </div>
-      </TableRow>
+        </div> */}
+        <Menus.Menu>
+          <Menus.Toggle id={cabinId} />
+          <Menus.List id={cabinId}>
+            <Menus.Button onClick={handleDuplicate} icon={<HiSquare2Stack />}>
+              Duplicate
+            </Menus.Button>
+            <Menus.Button
+              onClick={() => setShowForm(!showForm)}
+              icon={<HiPencil />}
+            >
+              Edit
+            </Menus.Button>
+            <Menus.Button
+              onClick={() => {
+                setShowDeleteModal(true);
+              }}
+              icon={<HiTrash />}
+            >
+              Delete
+            </Menus.Button>
+          </Menus.List>
+        </Menus.Menu>
+      </Table.Row>
 
       {showForm && (
-        <Modal onClose={()=>setShowForm(false)}>
-          <CreateCabinForm cabinToEdit={cabin} onCloseModel={()=>setShowForm(false)} />
+        <Modal onClose={() => setShowForm(false)}>
+          <CreateCabinForm
+            cabinToEdit={cabin}
+            onCloseModel={() => setShowForm(false)}
+          />
         </Modal>
       )}
-      {showDeleteModal &&(
-        <Modal onClose={()=>setShowDeleteModal(false)}>
-          <ConfirmDelete resourceName={'cabins'} disabled={isDeleting} onConfirm={()=>deleteCabin(cabinId)} onCloseModal={()=>setShowDeleteModal(false)}/>
+      {showDeleteModal && (
+        <Modal onClose={() => setShowDeleteModal(false)}>
+          <ConfirmDelete
+            resourceName={"cabins"}
+            disabled={isDeleting}
+            onConfirm={() => deleteCabin(cabinId)}
+            onCloseModal={() => setShowDeleteModal(false)}
+          />
         </Modal>
       )}
     </>
--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import { HiEllipsisVertical } from "react-icons/hi2";
 import styled from "styled-components";
 
-const StyledMenu = styled.div`
+const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -27,7 +31,6 @@ const StyledToggle = styled.button`
 
 const StyledList = styled.ul`
   position: fixed;
-
   background-color: var(--color-grey-0);
   box-shadow: var(--shadow-md);
   border-radius: var(--border-radius-md);
@@ -60,3 +63,83 @@ const StyledButton = styled.button`
     transition: all 0.3s;
   }
 `;
+
+const MenusContext = createContext();
+export default function Menus({ children }) {
+  const [openId, setOpenId] = useState("");
+  const [position, setPosition] = useState(null);
+  const menuRef = useRef(null);
+
+  const close = () => setOpenId("");
+  const open = setOpenId;
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        close();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <MenusContext.Provider
+      value={{ openId, close, open, position, setPosition, menuRef }}
+    >
+      {children}
+    </MenusContext.Provider>
+  );
+}
+
+function Toggle({ id }) {
+  const { openId, close, open, setPosition } = useContext(MenusContext);
+  function handleClick(e) {
+    const rect = e.target.closest("button").getBoundingClientRect();
+    setPosition({
+      x: window.innerWidth - rect.width - rect.x,
+      y: rect.y + rect.height + 8,
+    });
+    openId === "" || openId !== id ? open(id) : close();
+  }
+  return (
+    <StyledToggle onClick={handleClick}>
+      <HiEllipsisVertical />
+    </StyledToggle>
+  );
+}
+
+function List({ id, children }) {
+  const { openId, position, menuRef } = useContext(MenusContext);
+  if (openId !== id) return null;
+
+  return createPortal(
+    <StyledList ref={menuRef} position={position}>
+      {children}
+    </StyledList>,
+    document.body
+  );
+}
+
+function Button({ children, icon, onClick }) {
+  const { close } = useContext(MenusContext);
+  function handleClick() {
+    onClick?.();
+    close();
+  }
+  return (
+    <li>
+      <StyledButton onClick={handleClick}>
+        {icon} <span>{children}</span>
+      </StyledButton>
+    </li>
+  );
+}
+
+Menus.Menu = Menu;
+Menus.Toggle = Toggle;
+Menus.List = List;
+Menus.Button = Button;
